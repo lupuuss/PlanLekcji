4eb14076aa73de0c4ab8a8261e5364d550ee446c
b-galazka <bartlomiejgalazka@zoho.eu>
2020-11-17 20:20:13 +0100

    Revert "Update SettingsActivity.java"
    This reverts commit 167e5a3e5a0a97bf032e95d1344c08ed796557e7.

--- app/src/main/java/ga/lupuss/planlekcji/ui/activities/SettingsActivity.java
+++ app/src/main/java/ga/lupuss/planlekcji/ui/activities/SettingsActivity.java
@@ -58,9 +58,27 @@ public final class SettingsActivity extends AppCompatPreferenceActivity {
                         case 2:
                             toast("Nie zostaniesz tutaj programistą :(");
                             break;
+
                         case 4:
                             toast("No serio idź sobie :/");
                             break;
+
+                        case 6:
+                            toast("Odwal się!");
+                            break;
+
+                        case 8:
+                            toast("NIC TAM NIE MA");
+                            break;
+
+                        case 10:
+                            toast("Nie ma dowodów na to, że Hitler wiedział o Holocauście...");
+                            break;
+
+                        case 12:
+                            toast("Tylko rasowo czysta Europa");
+                            break;
+
                     }
                 }
 
